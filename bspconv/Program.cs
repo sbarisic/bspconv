05348a6a449c03fb4aa639a99c524157d018f012
Saša Barišić <cartman300@net.hr>
2017-07-14 19:00:18 +0200

    Added project files

---
+++ bspconv/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Libraria;
+using Libraria.Serialization;
+using Libraria.Serialization.Formats;
+
+namespace bspconv {
+	class Program {
+		static void Main(string[] args) {
+		}
+	}
+}
