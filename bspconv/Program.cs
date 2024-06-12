882b903df6037df3573c98ab2449725e04a1f60e
Saša Barišić <sbarisic@vub.hr>
2024-06-12 22:18:19 +0200

    Ability to convert single BSP in PK3 files

--- bspconv/Program.cs
+++ bspconv/Program.cs
@@ -8,24 +8,26 @@ using System.IO.Compression;
 using System.Collections.ObjectModel;
 using bspconv.Quake;
 using System.Runtime.InteropServices;
+using System.Web.UI;
 
 namespace bspconv {
 	class Program {
 		static void Main(string[] args) {
 			Console.Title = "Test3";
 
-			//args = new string[] { "46", "E:\\SteamLibrary\\steamapps\\common\\Quake Live\\baseq3\\pak00.pk3" };
+			//args = new string[] { "46", "E:\\SteamLibrary\\steamapps\\common\\Quake Live\\baseq3\\pak00.pk3", "--bsp:bloodrun.bsp" };
 
 			/*if (args.Length == 0)
-				args = new string[] { "maps_q3.pk3" };*/
+				args = new string[] { "bloodrun.pk3" };*/
 
 			QuakeVersion TargetVer = 0;
 			int StartIdx = 0;
 
 			if (args.Length == 0) {
-				Console.WriteLine("bspconv [version] file(.bsp|.pk3) [file2(.bsp|.pk3)]+");
+				Console.WriteLine("bspconv [version] file(.bsp|.pk3) [file2(.bsp|.pk3)]+ [--bsp:name]");
 				Console.WriteLine();
 				Console.WriteLine("\tversion - 46 (Quake 3) or 47 (Quake Live), output version. Input version any.");
+				Console.WriteLine("\t--bsp:bspname.bsp - Convert only bspname.bsp file in the provided .pk3 archive");
 				return;
 			}
 
@@ -39,7 +41,23 @@ namespace bspconv {
 
 
 			Console.WriteLine("Converting to IBSP {0} - {1}", (int)TargetVer, TargetVer);
+			string BSPName = null;
 
+
+			for (int i = StartIdx; i < args.Length; i++) {
+				if (args[i].StartsWith("--bsp:")) {
+					BSPName = args[i].Substring(6).Trim();
+
+					if (BSPName.Length == 0) {
+						Console.WriteLine("--bsp: switch needs to have bsp name filter (e.g. testmap.bsp)");
+						return;
+					}
+
+				} else if (args[i].StartsWith("--")) {
+					Console.WriteLine("Unknown switch {0}", args[i]);
+					return;
+				}
+			}
 
 			for (int i = StartIdx; i < args.Length; i++) {
 				string FileNameExt = args[i];
@@ -53,6 +71,8 @@ namespace bspconv {
 				string Extension = Path.GetExtension(FileNameExt);
 				string OutName = FileName + "_IBSP" + ((int)TargetVer) + Extension;
 
+				Console.WriteLine("Out file: {0}", Path.GetFullPath(OutName));
+
 				if (Extension == ".bsp") {
 					Console.WriteLine("Converting {0}", FileNameExt);
 
@@ -63,7 +83,7 @@ namespace bspconv {
 				} else if (Extension == ".pk3") {
 					OpenZip(FileNameExt, true, (In) => {
 						OpenZip(OutName, false, (Out) => {
-							ConvertFromPK3(TargetVer, In, Out);
+							ConvertFromPK3(TargetVer, In, Out, BSPName);
 						});
 					});
 				} else {
@@ -72,9 +92,12 @@ namespace bspconv {
 			}
 		}
 
-		static void ConvertFromPK3(QuakeVersion TargetVer, ZipArchive In, ZipArchive Out) {
+		static void ConvertFromPK3(QuakeVersion TargetVer, ZipArchive In, ZipArchive Out, string BSPName = null) {
 			int EntryNum = 0;
 			int Count = In.Entries.Count;
+			string BSPNameNoExt = Path.GetFileNameWithoutExtension(BSPName);
+
+
 
 			foreach (var Entry in In.Entries) {
 				Console.Title = string.Format("{0:0}%", ((float)EntryNum / Count) * 100);
@@ -83,7 +106,22 @@ namespace bspconv {
 				if (Entry.Length == 0)
 					continue;
 
+				string EntryFileName = Path.GetFileName(Entry.FullName);
+				string EntryFileNameNoExt = Path.GetFileNameWithoutExtension(Entry.FullName);
 				string EntryExt = Path.GetExtension(Entry.FullName);
+
+				if (BSPName != null) {
+					if (EntryExt == ".bsp" && EntryFileNameNoExt != BSPNameNoExt) {
+						Console.WriteLine("Skipping {0}", Entry.FullName);
+						continue;
+					}
+
+					if (EntryExt == ".aas" && EntryFileNameNoExt != BSPNameNoExt) {
+						Console.WriteLine("Skipping {0}", Entry.FullName);
+						continue;
+					}
+				}
+
 				ZipArchiveEntry OutEntry = Out.CreateEntry(Entry.FullName, CompressionLevel.Optimal);
 
 				OpenEntry(Entry, true, (InStream) => {
